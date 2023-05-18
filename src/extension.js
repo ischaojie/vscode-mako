006caac3195b6e05beb68a381ce34b25302d2d0a
Chaojie <hi@chaojie.fun>
2023-05-18 16:27:54 +0800

    feat: support vscode html features using htmlLanguageParticipants and if condition snippets

--- src/extension.js
+++ src/extension.js
@@ -3,18 +3,16 @@ const vscode = require('vscode')
 // This hack is necessary to ensure that the vscode.html-language-features extension is activated in order to enable
 // htmlLanguageParticipants support. Please see https://github.com/microsoft/vscode/issues/160585 for more info.
 async function activate() {
+  const output = vscode.window.createOutputChannel('Mako')
   const htmlExtension = vscode.extensions.getExtension('vscode.html-language-features')
-
   if (!htmlExtension) {
-    const output = vscode.window.createOutputChannel('Mako')
-
     output.appendLine(
       'Warning: Could not find vscode.html-language-features. HTML Language Participants support will be disabled.',
     )
     return
   }
-
   await htmlExtension?.activate()
+  output.appendLine('HTML Language Participants support enabled.')
 }
 
 module.exports = {
